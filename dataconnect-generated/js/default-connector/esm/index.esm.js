539c8684424a8320eac1885e15e6d59d99eb3909
PALLAVIMJHADAV <pallavijhadav1@gmail.com>
2025-05-11 18:15:04 +0530

    initial commit

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'serverless-forum',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'serverless-forum',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
